c6de1b18d7e1eb0691835eae8334999dcef34d23
rong <1141591465@qq.com>
2020-11-14 21:42:49 +0800

    add more doc

--- pkg/id/demo_test.go
+++ pkg/id/demo_test.go
@@ -5,13 +5,13 @@ package id_test
 import "log"
 
 //  example for generate id
-func Example_GenerateSlugID() {
+func ExampleGenerateSlugID() {
 	slug := GenerateSlugID("荣锋亮的测试博客")
 	log.Println(slug)
 }
 
 //  example for generate id
-func Example_GenerateSlugIDLang() {
+func ExampleGenerateSlugIDLang() {
 	slug := GenerateSlugIDLang("荣锋亮的测试博客", "en")
 	log.Println(slug)
 }
