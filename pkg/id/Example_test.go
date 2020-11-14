9bf814a24bac4680b05b1e6adca86bc53ea26399
rong <1141591465@qq.com>
2020-11-14 21:18:47 +0800

    add test

--- pkg/id/Example_test.go
+++ pkg/id/Example_test.go
@@ -9,3 +9,9 @@ func Example_GenerateSlugID() {
 	slug := GenerateSlugID("荣锋亮的测试博客")
 	log.Println(slug)
 }
+
+//  example for generate id
+func Example_GenerateSlugID() {
+	slug := GenerateSlugID("荣锋亮的测试博客")
+	log.Println(slug)
+}
